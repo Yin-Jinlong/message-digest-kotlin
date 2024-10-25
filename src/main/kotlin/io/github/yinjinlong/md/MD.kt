756bb79bad7bb7b95d9bf891344e94462febd02e
YJL <yjl_1567@qq.com>
2024-10-25 23:21:55 +0800

    添加缓存

--- src/main/kotlin/io/github/yinjinlong/md/MD.kt
+++ src/main/kotlin/io/github/yinjinlong/md/MD.kt
@@ -10,34 +10,47 @@ object MD {
 
     private val reg = Regex("^([A-Z]+)(\\d+)")
 
+    private val cachedName = mutableMapOf<String, String>()
+
+    var DefaultBufferSize = 4096
+
+    operator fun get(algorithm: String): MessageDigest? {
+        return MessageDigest.getInstance(algorithm)
+    }
+
     operator fun getValue(thisRef: Any?, property: KProperty<*>): MessageDigest {
-        // A:算法 V:算法版本 S:尺寸
-        // AV-S1/S2... =>
-        // v1 => AS1_S2...
-        // v[2-9] => AV_S1_S2...
+        return MessageDigest.getInstance(cachedName[property.name] ?: run {
+            // A:算法 V:算法版本 S:尺寸
+            // AV-S1/S2... =>
+            // v1 => AS1_S2...
+            // v[2-9] => AV_S1_S2...
 
-        val name = property.name.uppercase()
-        val parts = name.split("_")
+            val name = property.name.uppercase()
+            val parts = name.split("_")
 
-        val r1 = reg.find(parts[0]) ?: throw RuntimeException("Invalid property name: $name")
+            val r1 = reg.find(parts[0]) ?: throw RuntimeException("Invalid property name: $name")
 
-        val sList = if (parts.size > 1)
-            parts.subList(1, parts.size)
-        else
-            arrayListOf()
+            val sList = if (parts.size > 1)
+                parts.subList(1, parts.size)
+            else
+                arrayListOf()
 
-        val a = r1.groupValues[1]
-        val vOrS = r1.groupValues[2]
-        // v1 直接跟尺寸
-        if (vOrS.length > 1) {
-            val l = if (sList.isEmpty()) "" else "/"
-            val ss = sList.joinToString("/")
-            return MessageDigest.getInstance("$a-$vOrS$l$ss")
-        }
+            val a = r1.groupValues[1]
+            val vOrS = r1.groupValues[2]
+            // v1 直接跟尺寸
+            if (vOrS.length > 1) {
+                val l = if (sList.isEmpty()) "" else "/"
+                val ss = sList.joinToString("/")
+                "$a-$vOrS$l$ss"
+            } else {
+                val l = if (sList.isEmpty()) "" else "-"
+                val ss = sList.joinToString("/")
+                "$a$vOrS$l$ss"
+            }.apply {
+                cachedName[property.name] = this
+            }
+        })
 
-        val l = if (sList.isEmpty()) "" else "-"
-        val ss = sList.joinToString("/")
-        return MessageDigest.getInstance("$a$vOrS$l$ss")
     }
 
     val md2 by this
